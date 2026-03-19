d203fc0a76fbd08da39b4a5907fc341c14ee9961
Ivan Podymov <podymovivan74@gmail.com>
2026-03-19 19:48:08 +0300

    Добавить поддержку групп в модели экзаменов и формы. Обновить шаблоны для отображения групп и участников. Реализовать функции для управления группами и редактирования экзаменов.

--- exams/models.py
+++ exams/models.py
@@ -1,11 +1,12 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
 
 class Exam(models.Model):
     title = models.CharField(max_length=200, verbose_name="Название")
     date = models.DateTimeField(verbose_name="Дата проведения")
     image = models.ImageField(upload_to='exams/', verbose_name="Картинка", blank=True, null=True)
     users = models.ManyToManyField(User, related_name='exams', verbose_name="Пользователи", blank=True)
+    groups = models.ManyToManyField(Group, related_name='exams', verbose_name="Группы", blank=True)
     is_public = models.BooleanField(default=False, verbose_name="Опубликован")
 
     def __str__(self):
